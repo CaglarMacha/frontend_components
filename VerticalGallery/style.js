4c9ff7fd2c0d5e05c751e359f2741121a22ba865
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-11-25 18:21:17 +0300

    purchase button was added

--- VerticalGallery/style.js
+++ VerticalGallery/style.js
@@ -1,3 +1,5 @@
+
+
 const sliderContainer = document.querySelector(".slider-container");  
 const slideRight = document.querySelector(".right-slide");  
 const slideLeft = document.querySelector(".left-slide");  
